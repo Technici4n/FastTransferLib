1c4f219155716dab9fcf49bb77a56825f174f5fb
Bruno Ploumhans <bruno.ploumhans@gmail.com>
2020-11-26 23:14:31 +0100

    Adjust version semantics

--- src/main/java/dev/technici4n/fasttransferlib/api/item/ItemView.java
+++ src/main/java/dev/technici4n/fasttransferlib/api/item/ItemView.java
@@ -15,15 +15,15 @@ public interface ItemView {
     /**
      * Return the stack in some slot. Note that the stack may have an arbitrary count. It is possible that the returned stack may change
      *
-     * <p><b>THIS IS AN ITEM VIEW. DO NOT EVER MODIFY THE RETURNED SLOT.
+     * <p><b>THIS IS AN ITEM VIEW. DO NOT EVER MODIFY THE RETURNED STACK.
      * @param slot The slot id, must be between 0 and {@link ItemView#getSlotCount()}.
      * @throws IndexOutOfBoundsException if the slot is not in the range [0, {@link ItemView#getSlotCount()}).
      */
     ItemStack getStack(int slot);
 
     /**
-     * Return the version of this inventory. This number must be non-decreasing, and if this number is the same for two calls, it is expected
-     * that the underlying inventory hasn't changed. There is however no guarantee that the inventory has changed if this number has increased.
+     * Return the version of this inventory. f this number is the same for two calls, it is expected
+     * that the underlying inventory hasn't changed. There is however no guarantee that the inventory has changed if this number has changed.
      */
     default int getVersion() {
         return ItemImpl.version++;
